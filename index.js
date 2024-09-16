42d22b433a1870605e7db195189b4ba53ca0c15c
nihadkuluzade <nihadquluzade2001@gmail.com>
2024-09-16 16:27:51 +0400

    w2

---
+++ index.js
@@ -0,0 +1,35 @@
+let admin, name;
+name = "John";
+admin = name;
+alert( admin )
+
+
+
+const birthday = '18.04.1982';
+const age = someCode(birthday);
+
+
+
+const BIRTHDAY = '18.04.1982';
+const AGE = someCode(BIRTHDAY);
+
+
+
+let namee = "Ilya";
+alert( `hello ${1}` );
+alert( `hello ${"namee"}` );
+alert( `hello ${name}` );
+
+
+
+
+let a = 1, b = 1;
+alert( ++a ); 
+alert( b++ ); 
+alert( a ); 
+alert( b );
+
+
+
+
+
